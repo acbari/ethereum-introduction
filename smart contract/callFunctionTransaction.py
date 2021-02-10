3e74d86c99037939710e909f9f9f4a3443bfa9c9
akbari <acbari@users.noreply.github.com>
2021-02-10 15:14:42 +0700

    Update callFunctionTransaction.py

--- smart contract/callFunctionTransaction.py
+++ smart contract/callFunctionTransaction.py
@@ -1,7 +1,7 @@
 from web3 import Web3
 from web3.auto.infura import w3
 from web3.middleware import geth_poa_middleware
-from owner_module import ABI, BC
+from contract import compileContract
 import json
 
 w3 = Web3(Web3.HTTPProvider('<put your infura_project_id here>'))
@@ -10,6 +10,8 @@ w3.middleware_onion.inject(geth_poa_middleware, layer=0)
 myAdr = '0xa5C64A61D225Bc5614a2CE5fAc81926438e93844'
 private_key = '<put your private_key here>'
 nonce = w3.eth.getTransactionCount(myAdr)
+
+abi, bc = compileContract("solc-windows.exe","owner.sol")
 abi = json.loads(ABI)
 myContract = w3.eth.contract(address="0x5F06b8d51Cd337d48913C365000CA3b1465dAbA0", abi=abi)
 
