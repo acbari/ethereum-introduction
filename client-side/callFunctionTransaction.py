c326222148f3a114ace53224fc84904c759a4035
akbari <acbari@users.noreply.github.com>
2023-01-12 17:49:53 +0700

    Update callFunctionTransaction.py

--- client-side/callFunctionTransaction.py
+++ client-side/callFunctionTransaction.py
@@ -1,33 +1,41 @@
 from web3 import Web3
-from web3.auto.infura import w3
+from web3.auto import w3
 from web3.middleware import geth_poa_middleware
-from contract import compileContract
 import json
+from fwev_abi import ABI
 
 w3 = Web3(Web3.HTTPProvider('<put your infura_project_id here>'))
 w3.middleware_onion.inject(geth_poa_middleware, layer=0)
 
-myAdr = '0xa5C64A61D225Bc5614a2CE5fAc81926438e93844'
-private_key = '<put your private_key here>'
-nonce = w3.eth.getTransactionCount(myAdr)
+myAdr2 = '<put your account_2 address here>'
+private_key2 = '<put the private_key of the account_2 here>'
+nonce = w3.eth.getTransactionCount(myAdr2)
 
-abi, bc = compileContract("solc-windows.exe","owner.sol")
-abi = json.loads(abi)
-myContract = w3.eth.contract(address="0x5F06b8d51Cd337d48913C365000CA3b1465dAbA0", abi=abi)
+abi = ABI
+scadr = Web3.toChecksumAddress("<put your smart contract address here>")
+myContract = w3.eth.contract(address=scadr, abi=abi)
 
 transaction = {
-    'from': myAdr,
+    'from': myAdr2,
     'value': 0,
     'gas': 2000000,
-    'gasPrice': 1000000000,
+    'gasPrice': 2500000000,
     'nonce': nonce,
-    'chainId': 4
+    'chainId': 11155111
 }
+#chain Id: 11155111 => sepolia test network
 
-deploy_txn = myContract.functions.changeOwner('0x1C49AA3FEAb57bc27Ad79F0De64862786766F611').buildTransaction(transaction)
+pk2 = "<put the RSA public key for account 2 here>"
+deploy_txn = myContract.functions.req_connection(123,pk2).buildTransaction(transaction)
+#req_connection function has two parameters: req_id and req_PK
+
 signed_txn = w3.eth.account.sign_transaction(deploy_txn, private_key=private_key)
 txn_hash = w3.eth.sendRawTransaction(signed_txn.rawTransaction)
 print(txn_hash)
-txn_receipt = w3.eth.waitForTransactionReceipt(txn_hash)
-print(txn_receipt)
-print(txn_receipt['contractAddress'])
+
+#theese codes bellow generated error to INFURA API: 429 Client Error: Too Many Requests for url
+#we are missing a method to get the transaction status, whether it is succesfully recorded/failed?
+
+#txn_receipt = w3.eth.waitForTransactionReceipt(txn_hash)
+#print(txn_receipt)
+#print(txn_receipt['contractAddress'])
