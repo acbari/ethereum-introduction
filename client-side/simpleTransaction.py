2c663f28139791e3f3916dd9336ae8aa95a6a1f0
akbari <acbari@users.noreply.github.com>
2023-01-12 18:13:54 +0700

    Update simpleTransaction.py

--- client-side/simpleTransaction.py
+++ client-side/simpleTransaction.py
@@ -1,27 +1,28 @@
 from web3 import Web3
-from web3.auto.infura import w3
+from web3.auto import w3
 from web3.middleware import geth_poa_middleware
 
 w3 = Web3(Web3.HTTPProvider('<put your infura_project_id here>'))
 w3.middleware_onion.inject(geth_poa_middleware, layer=0)
 
-myAdr = '0xa5C64A61D225Bc5614a2CE5fAc81926438e93844'
-
+myAdr = '<put your account address here>'
+private_key = '<put the private_key of your account here>'
 nonce = w3.eth.getTransactionCount(myAdr)
 
 transaction = {
     'from': myAdr,
-    'to': '0x1C49AA3FEAb57bc27Ad79F0De64862786766F611',
+    'to': '<put the destination address here>',
     'value': 1000000000,
     'gas': 2000000,
-    'gasPrice': 1000000000,
+    'gasPrice': 2500000000,
     'nonce': nonce,
-    'chainId': 4
+    'chainId': 11155111
 }
-private_key = '<put your private_key here>'
+
 signed_tx = w3.eth.account.signTransaction(transaction, private_key)
 print("Sending transaction...")
 txn_hash = w3.eth.sendRawTransaction(signed_tx.rawTransaction)
-print(txn_hash)
-txn_receipt = w3.eth.waitForTransactionReceipt(txn_hash)
-print(txn_receipt)
+print(txn_hash.hex())
+
+#txn_receipt = w3.eth.waitForTransactionReceipt(txn_hash)
+#print(txn_receipt)
