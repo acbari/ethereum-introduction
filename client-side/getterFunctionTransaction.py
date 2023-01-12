649859c8659183d6975ae9b6cb3f20f77884365e
akbari <acbari@users.noreply.github.com>
2023-01-12 17:05:58 +0700

    Update getterFunctionTransaction.py

--- client-side/getterFunctionTransaction.py
+++ client-side/getterFunctionTransaction.py
@@ -1,28 +1,19 @@
 from web3 import Web3
-from web3.auto.infura import w3
+from web3.auto import w3
 from web3.middleware import geth_poa_middleware
-from contract import compileContract
 import json
+from fwev_abi import ABI   #fwev_abi.py has ABI variable that contain the ABI of the smart contract 
 
 w3 = Web3(Web3.HTTPProvider('<put your infura_project_id here>'))
 w3.middleware_onion.inject(geth_poa_middleware, layer=0)
 
-myAdr = '0xa5C64A61D225Bc5614a2CE5fAc81926438e93844'
+myAdr = '<put your account address here>'
 private_key = '<put your private_key here>'
 nonce = w3.eth.getTransactionCount(myAdr)
 
-abi, bc = compileContract("solc-windows.exe","owner.sol")
-abi = json.loads(abi)
-myContract = w3.eth.contract(address="0x5F06b8d51Cd337d48913C365000CA3b1465dAbA0", abi=abi)
+abi = ABI
+myContract = w3.eth.contract(address="<put your deployed smart contract address here>", abi=abi)
 
-transaction = {
-    'from': myAdr,
-    'value': 0,
-    'gas': 2000000,
-    'gasPrice': 1000000000,
-    'nonce': nonce,
-    'chainId': 4
-}
+txn_receipt = myContract.functions.owner().call() #change owner() function with the one that you want call
 
-txn_receipt = myContract.functions.getOwner().call()
 print(txn_receipt)
